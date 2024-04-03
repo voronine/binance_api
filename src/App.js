9ee01b9eec694da0c2757e10cc44484ab0b8b0ad
Yevhenii Voronin <voronine@ukr.net>
2024-04-03 22:31:31 +0300

    separating_file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { IgrFinancialChart, IgrFinancialChartModule } from 'igniteui-react-charts';
 import { Loader } from './Loader';
+import { fetchData } from './api';
 
 IgrFinancialChartModule.register();
 
@@ -10,96 +11,8 @@ function App() {
   const [indexSplit, setIndexSplit] = useState([]);
   const [indexDiv, setIndexDiv] = useState([]);
 
-  const intervalLimits = {
-    '1m': 24 * 60,
-    '5m': 24 * 60 * 7,
-    '15m': 24 * 60 * 30,
-    '30m': 24 * 60 * 30,
-    '1h': 24 * 60 * 30,
-    '1d': 24 * 60 * 30,
-    '1w': 24 * 60 * 30,
-    '1M': 24 * 60 * 30
-  };
-
-  const fetchData = async () => {
-    const burl = 'https://api.binance.com';
-    const query = `/api/v1/klines?symbol=BTCUSDT&interval=${interval}&limit=${intervalLimits[interval]}`;
-    const url = burl + query;
-
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const jsonData = await response.json();
-
-      if (Array.isArray(jsonData) && jsonData.length > 0) {
-        const processedData = processData(jsonData);
-        setCandleData(processedData);
-      } else {
-        console.error('Empty or invalid data received');
-      }
-    } catch (error) {
-      console.error('Fetch error:', error);
-    }
-};
-
-  const processData = candles => {
-    let minClose = 0;
-    let maxClose = 0;
-    let minIndex = 0;
-    let maxIndex = 0;
-    let indexSplit = [];
-    let indexDiv = [];
-
-    const processedData = candles.map((candle, index) => {
-        const candleData = {
-            Date: new Date(candle[0]),
-            Open: parseFloat(candle[1]),
-            High: parseFloat(candle[2]),
-            Low: parseFloat(candle[3]),
-            Close: parseFloat(candle[4]),
-            Volume: parseFloat(candle[5])
-        };
-
-        if (candleData.Close < minClose) {
-            minClose = candleData.Close;
-            minIndex = index;
-        }
-        if (candleData.Close > maxClose) {
-            maxClose = candleData.Close;
-            maxIndex = index;
-        }
-
-        if (indexSplit.length < 5 && Math.random() < 0.1) {
-          const randomIndex = Math.floor(Math.random() * candles.length);
-          indexSplit.push(randomIndex);
-        }
-      
-        if (indexDiv.length < 5 && Math.random() < 0.1) {
-          const randomIndex = Math.floor(Math.random() * candles.length);
-          indexDiv.push(randomIndex);
-        }
-
-        return candleData;
-    });
-
-    indexSplit.forEach(idx => {
-      processedData[idx].info = `Sell ↓ ${processedData[idx].Close}`;
-  });
-  indexDiv.forEach(idx => {
-      processedData[idx].info = `Buy ↑ ${processedData[idx].Close}`;
-  });
-
-    processedData[minIndex].info = "MIN";
-    processedData[maxIndex].info = "MAX";
-
-    return processedData;
-};
   useEffect(() => {
-    fetchData();
-    setIndexSplit([]);
-    setIndexDiv([]);
+    fetchData(interval, setCandleData, setIndexSplit, setIndexDiv);
   }, [interval]);
 
   const handleIntervalChange = e => {
